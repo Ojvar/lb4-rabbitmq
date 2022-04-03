ce44e2999d33a40fe4f2057b8eea6868b6784c5e
Ojvar <MOjvar.AmirHossein@gmail.com>
2022-04-03 13:39:50 +0430

    Update types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,10 @@
 import {BindingTemplate, ContextTags, extensionFor} from '@loopback/core';
-import amqplib, {ConsumeMessage, Options} from 'amqplib';
+import amqplib from 'amqplib';
 import {RabbitmqServiceKeys} from './keys';
 
 export namespace RabbitmqServiceTypes {
   export type RabbitmqConfig = amqplib.Options.Connect;
+  export type ConsumeMessage = amqplib.ConsumeMessage;
 
   export const asConsumer: BindingTemplate = binding => {
     extensionFor(RabbitmqServiceKeys.RABBITMQ_SERVICE_EXTENSION_POINT)(binding);
@@ -16,8 +17,8 @@ export namespace RabbitmqServiceTypes {
   export interface Consumer {
     name?: string;
     queue: string;
-    consumeOptions?: Options.Consume;
+    consumeOptions?: amqplib.Options.Consume;
 
-    handler(msg: ConsumeMessage | null): void;
+    handler(msg: amqplib.ConsumeMessage | null): void;
   }
 }
