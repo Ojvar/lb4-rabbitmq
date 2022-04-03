e52bb68647a19c738aa20049658588c2756f1dd8
Ojvar <MOjvar.AmirHossein@Gmail.com>
2022-04-03 23:00:04 +0430

    Add lifecycle to rabbitmq-component

--- src/components/rabbitmq.component.ts
+++ src/components/rabbitmq.component.ts
@@ -3,17 +3,40 @@ import {
   Component,
   ContextTags,
   createBindingFromClass,
+  inject,
+  lifeCycleObserver,
+  LifeCycleObserver,
 } from '@loopback/core';
+import debugFactory from 'debug';
 import {ConsumersBooter} from '../booters';
 import {RabbitmqServiceKeys} from '../keys';
 import {RabbitmqService} from '../services';
 
-export class RabbitmqComponent implements Component {
-  booters = [ConsumersBooter];
+const trace = debugFactory('RabbitmqService:RabbitmqComponent');
 
+export class RabbitmqComponent implements Component {
+  lifeCycleObservers = [RabbitmqObjserver];
+  booters = [ConsumersBooter];
   bindings?: Binding<unknown>[] | undefined = [
     createBindingFromClass(RabbitmqService, {
       [ContextTags.KEY]: RabbitmqServiceKeys.RABBITMQ_SERVICE,
     }),
   ];
+}
+
+@lifeCycleObserver()
+export class RabbitmqObjserver implements LifeCycleObserver {
+  constructor(
+    @inject(RabbitmqServiceKeys.RABBITMQ_SERVICE)
+    private rabbitmqService: RabbitmqService,
+  ) {}
+
+  async start() {
+    trace('Lifecycle -> Start');
+    await this.rabbitmqService.connect();
+  }
+
+  async stop() {
+    trace('Lifecycle -> Stop');
+  }
 }
